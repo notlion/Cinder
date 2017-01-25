1e5a4b3db6e41d2dc4f203a92488faded9a3fb85
Ryan Alexander <ryan@onecm.com>
2017-01-25 14:37:07 -0800

    Fix shader memory leak in GlslProg
    Calling `glDeleteShader` on a shader after it is attached to a program flags it for deletion once it is no longer attached (e.g. When the program is deleted).

--- src/cinder/gl/GlslProg.cpp
+++ src/cinder/gl/GlslProg.cpp
@@ -696,6 +696,7 @@ void GlslProg::loadShader( const string &shaderSource, const fs::path &shaderPat
 		throw GlslProgCompileExc( log, shaderType );
 	}
 	glAttachShader( mHandle, handle );
+	glDeleteShader( handle );
 }
 
 void GlslProg::link()
